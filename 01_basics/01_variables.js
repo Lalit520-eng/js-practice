b5cb079f4dd20881f3db649dc895b6b894a498c4
Lalit520-eng <83266840+Lalit520-eng@users.noreply.github.com>
2025-02-11 11:12:20 +0000

    variables and datatypes example

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,3 +8,9 @@ because of issues in block space and functional scope
 var password = "test@123";
 var password ="123@test"
 console.table([id,name,email,password])
+
+let num1,num2,num3;
+num1 = num2=num3=2+2;
+console.log("num1 " + num1)
+console.log("num2 " + num2)
+console.log("num3 " + num3)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+/* 
+Primitive data types / Value Types
+string, number,boolean, null, undefined, bigint, symbol
+*/
+
+const name = "Lalit";
+const age = 48;
+let place ="danya";
+
+const number =123.896;
+const convertnumber = number.toFixed(2);
+console.log((typeof convertnumber));
+
+// answer will be 123.90 and it will return string
+console.log(number.toFixed(2));
+
+
+
+//console.log(`my name is ${name} and my age is ${age} and i am from ${place}` );
+
+const myName = new String("lalit_mohan_joshi");
+const myNameArray = myName.split('_');
+//console.log(myNameArray)
+
+
+/* 
+ Non-Primitive data types / Refrence Types
+ array, object, functions
+*/
+
+const heros = ["Shaktiman","Nagraj","Dogra"];
+
+let myObje = {
+    name:"lalit",
+    age: 38,
+}
