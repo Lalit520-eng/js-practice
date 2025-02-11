27632540b86350a87d7aa2936788e62142adcb05
Lalit520-eng <83266840+Lalit520-eng@users.noreply.github.com>
2025-02-11 07:56:18 +0000

    test variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const id = 12222;
+let name ="lalit";
+let email = "lalit.joshi@gmail.com";
+/*
+prefer not to use var
+because of issues in block space and functional scope
+*/
+var password = "test@123";
+var password ="123@test"
+console.table([id,name,email,password])
