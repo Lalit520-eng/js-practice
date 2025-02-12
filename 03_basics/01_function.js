a253d7c02814305a63ad9c570ec32ed7cf26cb47
Lalit520-eng <83266840+Lalit520-eng@users.noreply.github.com>
2025-02-12 05:23:51 +0000

    function example

---
+++ 03_basics/01_function.js
@@ -0,0 +1,9 @@
+/* this called rest operator ...num1 
+  which will handle more then one value
+*/
+function CalculatePriceFromCard(...num1)
+{
+   return num1;
+}
+
+console.log(CalculatePriceFromCard(10,20,30,40));
